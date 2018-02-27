8b5f32c639e053afdc1c0c4e25da21442a66e711
Krisz Magyar <krisz.magyar@gmail.com>
2018-02-27 17:09:06 +0100

    init pug

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+//Express init
 app.use(express.static('public'))
+app.set('view engine', 'pug')
 
-app.get('/', (req, res) => res.send('Landing page'))
+//Routes
+app.get('/', (req, res) => res.render('index'))
 
+//Server start
 app.listen(port, () =>
 	console.log('Server has started on port', port))
